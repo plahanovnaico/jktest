066007e9dd7887db5df3b99212351ee8651e5be8
KochuraniNaico <kochurani.k@naicoits.com>
2019-12-04 12:58:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express=require("express");
+const app=express();
+var port=process.env.PORT |'3000'
+app.get('/',(req,res)=> res.send ('hello world'));
+app.listen(port,()=> console.log("server is running on the port"+port));
